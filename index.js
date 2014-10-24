0c3db3a3ad78e68f4cf471ad5044394bd0c3f6f4
Wouter van Reeven <info@wwwappz.nl>
2014-10-24 16:49:19 +0200

    Nieuw file toevoegen Verwijderen buzzer database Toevoegen

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 var Gpio = require('onoff').Gpio,
   pir = new Gpio(17, 'in', 'both');
+var sqlite3 = require('sqlite3').verbose();
+var db;
+
+function createDb() {
+    console.log("createDb chain");
+    db = new sqlite3.Database('chain.sqlite3', createTable);
+}
+
+function createTable() {
+    console.log("createTable lorem");
+    db.run("CREATE TABLE IF NOT EXISTS teller (datum datetime default CURRENT_TIMESTAMP, tel TEXT)", insertRows);
+}
+
+function insertRows() {
+    console.log("insertRows");
+    var stmt = db.prepare("INSERT INTO teller (tel) VALUES (?)");
+    stmt.run(1);
+    stmt.finalize(readAllRows);
+}
+function readAllRows() {
+    console.log("readAllRows lorem");
+    db.all("SELECT * FROM teller", function(err, rows) {
+        console.log(rows.lenght)
+        closeDb();
+    });
+}
+
+function closeDb() {
+    console.log("closeDb");
+    db.close();
+}
+
  
 pir.watch(function(err, value) {
   if (err) exit();
   console.log('Intruder detected');
+  createDb();
 });
  
 console.log('Pi Bot deployed successfully!');
